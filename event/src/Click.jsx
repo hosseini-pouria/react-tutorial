48d8a64ee49553cb4429c55e044f276dc73df2ad
Hosseini.Pouria(ACT) <Hosseini.Pouria@actholding.local>
2024-02-05 19:14:44 +0330

    done

--- event/src/Click.jsx
+++ event/src/Click.jsx
@@ -1,10 +1,27 @@
 import React, {Component} from "react";
 export default class Click extends Component{
-    state = {
-        clickCount: 0
+    constructor(props) {
+        super(props);
+        this.state = {
+            clickCount: 0
+        }
+
+        this.handleClick = this.handleClick.bind(this)
     }
 
-    handleClick = (event) => {
+    render() {
+        return (
+            <div>
+                <p>Click count is {this.state.clickCount}</p>
+                <button onClick={this.handleClick}>Click Me!</button>
+                <div onClick={this.handleClick}>
+                    <a onClick={this.handleLink} href="http://7learn.ir">7learn</a>
+                </div>
+            </div>
+        );
+    }
+
+    handleClick(event){
         this.setState((state) => {
             return {
                 clickCount: state.clickCount + 1
@@ -12,29 +29,10 @@ export default class Click extends Component{
         })
     }
 
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         clickCount: 0
-    //     }
-    //
-    //     // this.handleClick = this.handleClick.bind(this)
-    // }
-
-    render() {
-        return (
-            <div>
-                <p>Click count is {this.state.clickCount}</p>
-                <button onClick={(e) => this.handleClick()}>Click Me!</button>
-            </div>
-        );
+    handleLink(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        console.log(event.target)
+        console.log('clicked')
     }
-
-    // handleClick(event){
-    //     this.setState((state) => {
-    //         return {
-    //             clickCount: state.clickCount + 1
-    //         }
-    //     })
-    // }
 }
