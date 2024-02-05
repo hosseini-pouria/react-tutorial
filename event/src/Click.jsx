90e6949206b140972c752efa165b71c51a66c64b
Hosseini.Pouria(ACT) <Hosseini.Pouria@actholding.local>
2024-02-05 18:15:50 +0330

    done

---
+++ event/src/Click.jsx
@@ -0,0 +1,40 @@
+import React, {Component} from "react";
+export default class Click extends Component{
+    state = {
+        clickCount: 0
+    }
+
+    handleClick = (event) => {
+        this.setState((state) => {
+            return {
+                clickCount: state.clickCount + 1
+            }
+        })
+    }
+
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         clickCount: 0
+    //     }
+    //
+    //     // this.handleClick = this.handleClick.bind(this)
+    // }
+
+    render() {
+        return (
+            <div>
+                <p>Click count is {this.state.clickCount}</p>
+                <button onClick={(e) => this.handleClick()}>Click Me!</button>
+            </div>
+        );
+    }
+
+    // handleClick(event){
+    //     this.setState((state) => {
+    //         return {
+    //             clickCount: state.clickCount + 1
+    //         }
+    //     })
+    // }
+}
